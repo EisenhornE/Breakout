fb683a5132e4ee7ed13e38c7404efa0cfd7cabda
Carl John Ignacio Nicor <cjiNicor@gmail.com>
2023-07-26 16:20:01 +0800

    refactor: changed the name of the StartGame Script feat: Added a quit button.

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -1,10 +1,15 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class StartGame : MonoBehaviour
+public class Button : MonoBehaviour
 {
     public void StartGameButton()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
     }
 }
