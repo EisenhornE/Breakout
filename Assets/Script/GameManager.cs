db52231a61b8bfe72254d7ae6740871a46a3aae1
Carl John Ignacio Nicor <cjiNicor@gmail.com>
2023-08-05 12:51:40 +0800

    feat: added a plus life mechanic to the game

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -20,10 +20,26 @@ public class GameManager : MonoBehaviour
         if (Lives == 0)
         {
             Debug.Log("Game Over");
-            BallLauncher ballLauncher = FindObjectOfType<BallLauncher>();
             PlayerController playerController = FindObjectOfType<PlayerController>();
             playerController.enabled = false;
             playerController.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    public void AddLife()
+    {
+        Lives++;
+        _livesText.text = "Lives: " + Lives;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
         }
     }
 }
--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -3,19 +3,30 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int score;
+    private static int score;
     private TextMeshProUGUI _scoreText;
+    private GameManager _gameManager;
+    private bool isDivisibleby100;
 
     void Start()
     {
         _scoreText = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
         score = 0;
         _scoreText.text = "Score: " + score;
+        _gameManager = FindObjectOfType<GameManager>();
     }
 
     public void AddScore(int points)
     {
         score += points;
         _scoreText.text = "Score: " + score;
+
+        isDivisibleby100 = score % 100 == 0;
+
+        if (isDivisibleby100 && score != 0)
+        {
+            _gameManager.AddLife();
+            Debug.Log("Added a life!");
+        }
     }
 }
